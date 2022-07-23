ce8df58b28f88ffb882cd5a242ffd72df7dfa2ee
waseemlinuxfreakz <waseem.linuxfreakz@gmail.com>
2022-07-23 20:38:54 +0500

    Updates

---
+++ precache-manifest.b94677b06593cdc7e4764de5554a5b71.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a76dcb0d84f2ee65cdb",
+    "url": "/flashreact/static/css/main.754ac4b5.chunk.css"
+  },
+  {
+    "revision": "1a76dcb0d84f2ee65cdb",
+    "url": "/flashreact/static/js/main.1a76dcb0.chunk.js"
+  },
+  {
+    "revision": "dca74a035a2640b5084a",
+    "url": "/flashreact/static/js/1.dca74a03.chunk.js"
+  },
+  {
+    "revision": "11a80160aa4f4cf1204c",
+    "url": "/flashreact/static/css/2.0e43170d.chunk.css"
+  },
+  {
+    "revision": "11a80160aa4f4cf1204c",
+    "url": "/flashreact/static/js/2.11a80160.chunk.js"
+  },
+  {
+    "revision": "a3704a1bec3d0704b82c",
+    "url": "/flashreact/static/js/runtime~main.a3704a1b.js"
+  },
+  {
+    "revision": "6e061f17f821c3051753548d0941cffd",
+    "url": "/flashreact/static/media/logo.6e061f17.svg"
+  },
+  {
+    "revision": "3ae45b7648be8e1bcd42c1d277ec42a4",
+    "url": "/flashreact/static/media/fb.3ae45b76.svg"
+  },
+  {
+    "revision": "f1cf723c45b0fbc99ada14c4dcc4c558",
+    "url": "/flashreact/static/media/inst.f1cf723c.svg"
+  },
+  {
+    "revision": "b5f2b34d8a6bf0008d8e8c4372e13460",
+    "url": "/flashreact/static/media/twi.b5f2b34d.svg"
+  },
+  {
+    "revision": "992b3b4fd196ac2a4de4585b7eb60e32",
+    "url": "/flashreact/static/media/you.992b3b4f.svg"
+  },
+  {
+    "revision": "4a04060c88e83321e6908d96f7be357d",
+    "url": "/flashreact/static/media/logo-2.4a04060c.svg"
+  },
+  {
+    "revision": "061c7e5cf6075fb411dcb9196efb2203",
+    "url": "/flashreact/static/media/banar.061c7e5c.png"
+  },
+  {
+    "revision": "1026df527213a99d6375895112ba6334",
+    "url": "/flashreact/index.html"
+  }
+];
