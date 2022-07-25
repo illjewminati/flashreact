cebacb40062c2a68767a27fd5d488dbc07c1fb72
illjewminati <103166826+illjewminati@users.noreply.github.com>
2022-07-25 19:42:13 -0400

    remove flash - add nogas

--- src/components/homeInner/QA.js
+++ src/components/homeInner/QA.js
@@ -9,7 +9,7 @@ function FAQ() {
                     <h2>Q & A</h2>
                     <Accordion defaultActiveKey="">
                         <Accordion.Item eventKey="0">
-                            <Accordion.Header>HOW CAN THE FLASH PROTOCOL HELP ME?</Accordion.Header>
+                            <Accordion.Header>HOW CAN THE NOGAS PROTOCOL HELP ME?</Accordion.Header>
                             <Accordion.Body>
                                 Users can privately and securely BUY/SELL/SWAP ERC20 tokens 
                                 without having to spend eth on gas ever again 
@@ -18,7 +18,7 @@ function FAQ() {
                         <Accordion.Item eventKey="1">
                             <Accordion.Header>DO I NEED CODING EXPERIENCE TO USE THESE TOOLS?</Accordion.Header>
                             <Accordion.Body>
-                            No you do not! and therein lies the beauty of the Flash ecosystem. 
+                            No you do not! and therein lies the beauty of the NOGAS ecosystem. 
                             We have already done the hard part of turning complex code into a simple 
                             UI that anybody can use
 
@@ -28,8 +28,7 @@ function FAQ() {
                             <Accordion.Header>HOW MUCH DO THESE UTILITIES COST?</Accordion.Header>
                             <Accordion.Body>
                             There will be different Tiers and utilities available according to 
-                            how much $TBD token you hold. These details will be released shortly
-                            after launch
+                            how much $TBD token you hold. These details will be released after launch
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
