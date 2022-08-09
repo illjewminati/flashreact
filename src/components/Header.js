d0284c59942637786cd456bfe95019906286b664
illjewminati <103166826+illjewminati@users.noreply.github.com>
2022-08-08 22:23:00 -0600

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import {FaTwitter,FaTelegram} from "react-icons/fa";
 // Logo
 import Logo from '../img/logo.svg';
 import { Link } from 'react-scroll';
@@ -20,6 +20,10 @@ function Header() {
                         <Link to="services" spy={true} smooth={true} offset={-120} duration={500}> <Nav.Link>Q & A</Nav.Link></Link>   
                           <Link to="tokenomics" spy={true} smooth={true} offset={-120} duration={500}><Nav.Link >TOKEN</Nav.Link></Link>  
                            <Nav.Link href="./litepaper/NOGAS V1 LITEPAPER.pdf" target="_blank"> LITEPAPER</Nav.Link>    
+                           <Nav.Link target="_blank" href="https://twitter.com/_nogas" > <FaTwitter className="header_social"/></Nav.Link>    
+
+                           <Nav.Link target="_blank" href="https://t.me/nogastoken" > <FaTelegram className="header_social"/></Nav.Link>    
+
                             <Nav.Link href="#" className='AppLaunce'>LAUNCH APP</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
