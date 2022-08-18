f363985d30d6ca6d88846aa14a193d0cb62e58b6
illjewminati <103166826+illjewminati@users.noreply.github.com>
2022-08-17 19:58:04 -0600

    Add files via upload

--- src/components/homeInner/TOKENOMICS.js
+++ src/components/homeInner/TOKENOMICS.js
@@ -8,7 +8,7 @@ function TOKENOMICS() {
                 <div className="row">
                     <div className="col-lg-4 col-md-6 col-sm-12" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="100">
                         <div className="token_box">
-                            <p>1,000,000,000,000</p>
+                            <p>100,000,000,000</p>
                             <h4>TOTAL TOKEN SUPPLY</h4>
                             <p>2% MAX WALLET <br />
                             .5% MAX TRANSACTION</p>
