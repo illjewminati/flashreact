5dc9ada84bcc348e38365048540f7ec2dc0d2b16
illjewminati <blockchainpapi69@gmail.com>
2022-09-01 21:47:46 -0600

    banner text

--- src/components/homeInner/Banar.js
+++ src/components/homeInner/Banar.js
@@ -9,7 +9,7 @@ function Banar() {
             <div className="container">
                 <div className="banar_content">
                     <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300">PROTOCOL <br />ZERO</h1>
-                    <p data-aos="fade-down" data-aos-duration="1000">The only place for 0 GAS trades on the <br />Ethereum Network</p>
+                    <p data-aos="fade-down" data-aos-duration="1000">The only place to make 0 GAS transactions on the <br />Ethereum Network</p>
                     <a href="https://www.dextools.io/app/ether/pair-explorer/0xa5fb965433feccc7e0d1089b02529804199b0588" target="_blank" className='banarBtn' data-aos="fade-up" data-aos-duration="1000">BUY ON DEXTOOLS</a>
                 </div>
             </div>
