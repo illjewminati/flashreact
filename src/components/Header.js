f1317745a96423fce3b03df5b87bed95e050f8be
illjewminati <Blockchainpapi69@gmail.com>
2023-02-26 11:55:59 +0700

    link edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,7 +50,7 @@ export default class Header extends Component {
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="AppLaunce nav-link" aria-current="page" href="https://www.swap.nogas.pro"  ref='noreferrer'>
+                                    <a className="AppLaunce nav-link" aria-current="page" href="https://swap.nogas.pro"  ref='noreferrer'>
                                         LAUNCH APP
                                     </a>
                                 </li>
