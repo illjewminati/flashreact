d98aadbdad0196471fad7c06f9ba938d67243fba
illjewminati <Blockchainpapi69@gmail.com>
2023-03-18 16:08:18 +0700

    npm build

---
+++ build/precache-manifest.a8bc20a09dbfbc75ef40164cc5893036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b315da9345461fbdc94d",
+    "url": "/static/js/main.b315da93.chunk.js"
+  },
+  {
+    "revision": "8e8e239ffeeeff146afc",
+    "url": "/static/js/1.8e8e239f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1d2e041835d47f5b51ea57cddab9345",
+    "url": "/static/media/banar.d1d2e041.png"
+  },
+  {
+    "revision": "ad09d40d61731157de2bc0a173fefb9c",
+    "url": "/static/media/logonotext.ad09d40d.png"
+  },
+  {
+    "revision": "e563f588c39fe21dea57ab35b890c8f7",
+    "url": "/index.html"
+  }
+];
