75ca409a6b906af0318d4c7f5e92b352b44d4b69
illjewminati <Blockchainpapi69@gmail.com>
2023-03-25 12:28:24 +0700

    typo

--- src/pages/home/components/Banar.js
+++ src/pages/home/components/Banar.js
@@ -11,7 +11,7 @@ function Banar() {
                     <div className="col-md-8">
                         <div className="banar_content">
                             <h1 data-aos="fade-down" data-aos-duration="1000" data-aos-delay="300">PROTOCOL <br />ZERO</h1>
-                            <p data-aos="fade-down" data-aos-duration="1000">Leveraging Account Abstraction via Private Relay to bring Gasless transactions to the Ethereum Network</p>
+                            <p data-aos="fade-down" data-aos-duration="1000">Leveraging Account Abstraction via Private Relayer to bring Gasless transactions to the Ethereum Network</p>
                             <a href="https://www.dextools.io/app/ether/pair-explorer/0xa5fb965433feccc7e0d1089b02529804199b0588" className='banarBtn' data-aos="fade-up" data-aos-duration="1000">BUY ON DEXTOOLS</a>
                         </div>
                     </div>
