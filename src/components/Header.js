04d8d35a919c0e1e7c737811a571926cd792ab03
illjewminati <Blockchainpapi69@gmail.com>
2023-03-26 15:36:27 +0700

    v2 web

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export default class Header extends Component {
                 <nav className="navbar navbar-expand-lg navbar-dark">
                     <div className="container">
                         <a className="navbar-brand" href="home">
-                            <img src={Logo} alt="Logo" width='70' height="90" />
+                            <img src={Logo} alt="Logo" width='90' height="90" />
                         </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
